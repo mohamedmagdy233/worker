fbc5e837518381245f9bccea719e018408e57edf
mohamedmagdy233 <mohamed.magedi.backend@gmail.com>
2026-03-06 15:24:18 +0200

    update-v2

--- p1p.php
+++ p1p.php
@@ -1,28 +1,17 @@
 <?php
 
+
 $pageId = '434309674008905';
+
+
 $token = 'EAARdg15lAY8BQ4POkyhj3beYt5YBDHWp2RaFW5MNr7oFSgiZBrSPDAtXi07vA6EVPafJLQ3YlS1LID5CW8OVSifUsR6X6XWnOcEV1aaceZBZA7OezQbxzISGP49zfiFuZBGkTLWqbwMbHLZBh7bje0E82XFgpZBJEZAYFeTPZAVdlZBYbP9BzhhYw8S95vyvQhzZCOprLr';
 
+$message = "إِنَّ اللَّهَ وَمَلَائِكَتَهُ يُصَلُّونَ عَلَى النَّبِيِّ ۚ يَا أَيُّهَا الَّذِينَ آمَنُوا صَلُّوا عَلَيْهِ وَسَلِّمُوا تَسْلِيمًا ﷺ.";
 
-$apiUrl = "https://api.alquran.cloud/v1/ayah/random/ar.quran-simple";
-$apiResponse = file_get_contents($apiUrl);
-$quranData = json_decode($apiResponse, true);
-
-if ($quranData['code'] !== 200) {
-    die("خطأ في جلب الآية.");
-}
-
-$ayahText = $quranData['data']['text'];
-$surahName = $quranData['data']['surah']['name'];
-$ayahNumber = $quranData['data']['numberInSurah'];
-
-$message = "{$ayahText}\n\n[{$surahName} - الآية {$ayahNumber}]";
-
-
-$fbUrl = "https://graph.facebook.com/v19.0/{$pageId}/feed";
+$url = "https://graph.facebook.com/v19.0/{$pageId}/feed";
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $fbUrl);
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
     'message' => $message,
@@ -32,5 +21,6 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
 curl_close($ch);
+
 
 echo $response;
--- p5p.php
+++ p5p.php
@@ -1,17 +1,28 @@
 <?php
 
-
 $pageId = '434309674008905';
-
-
 $token = 'EAARdg15lAY8BQ4POkyhj3beYt5YBDHWp2RaFW5MNr7oFSgiZBrSPDAtXi07vA6EVPafJLQ3YlS1LID5CW8OVSifUsR6X6XWnOcEV1aaceZBZA7OezQbxzISGP49zfiFuZBGkTLWqbwMbHLZBh7bje0E82XFgpZBJEZAYFeTPZAVdlZBYbP9BzhhYw8S95vyvQhzZCOprLr';
 
-$message = "اللهم صلِّ وسلم وبارك على نبينا محمد.";
 
-$url = "https://graph.facebook.com/v19.0/{$pageId}/feed";
+$apiUrl = "https://api.alquran.cloud/v1/ayah/random/ar.quran-simple";
+$apiResponse = file_get_contents($apiUrl);
+$quranData = json_decode($apiResponse, true);
+
+if ($quranData['code'] !== 200) {
+    die("خطأ في جلب الآية.");
+}
+
+$ayahText = $quranData['data']['text'];
+$surahName = $quranData['data']['surah']['name'];
+$ayahNumber = $quranData['data']['numberInSurah'];
+
+$message = "{$ayahText}\n\n[{$surahName} - الآية {$ayahNumber}]";
+
+
+$fbUrl = "https://graph.facebook.com/v19.0/{$pageId}/feed";
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $fbUrl);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
     'message' => $message,
@@ -21,6 +32,5 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
 curl_close($ch);
-
 
 echo $response;
